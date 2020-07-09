495311a19ac431627975a263d5a89a04400ce45c
walwe <git@walwe.de>
2020-07-09 10:24:04 +0200

    v0.0.1

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Backups django media and postgres db running in docker container.
 ```
 django_docker_backup.sh -m <media-volume-name> -c <db-container-name> -d <db-role> -o <output-dir>
 ```
+- Set `-m`  to backup media directory
+- Set `-c`  to backup database 
--- django_docker_backup.sh
+++ django_docker_backup.sh
@@ -7,8 +7,8 @@ function show_help {
   echo "$0 -m <media-volume-name> -c <db-container-name> -d <db-role> -o <output-dir>"
 }
 
-MEDIA_VOLUME_ROLE=""
-DB_CONTAINER_ROLE=""
+MEDIA_VOLUME_NAME=""
+DB_CONTAINER_NAME=""
 DB_ROLE=postgres
 
 while getopts "h?d:m:c:o:" opt; do
@@ -17,11 +17,11 @@ while getopts "h?d:m:c:o:" opt; do
         show_help
         exit 0
         ;;
-    m) MEDIA_VOLUME_ROLE=${OPTARG}
+    m) MEDIA_VOLUME_NAME=${OPTARG}
         ;;
     d) DB_ROLE=${OPTARG}
         ;;
-    c) DB_CONTAINER_ROLE=${OPTARG}
+    c) DB_CONTAINER_NAME=${OPTARG}
         ;;
     o) OUTPUT_PATH=${OPTARG}
         ;;
@@ -32,19 +32,25 @@ shift $((OPTIND-1))
 
 [ "${1:-}" = "--" ] && shift
 
-if [ -z "${MEDIA_VOLUME_ROLE}" ] || [ -z "${DB_CONTAINER_ROLE}" ] || [ -z "${OUTPUT_PATH}" ]; then
+if [ -z "${OUTPUT_PATH}" ] || [[ ( -z "${MEDIA_VOLUME_NAME}" )  &&  ( -z "${DB_CONTAINER_NAME}" ) ]]; then
     show_help
     exit 1
 fi
 
-
-DATE_NOW=$(date -u +"%Y-%m-%dT%H_%M_%S")
-MEDIA_ARCHIVE="${OUTPUT_PATH}/media_${DATE_NOW}.zip"
-SQL_ARCHIVE="${OUTPUT_PATH}/db_dump_${DATE_NOW}.sql.gz"
-
-MEDIA_VOLUME_PATH=$(docker volume inspect ${MEDIA_VOLUME_ROLE} | sed -ne '/"Mountpoint": "/p' | sed 's/.*"\(\/.*\)".*/\1/')
 mkdir -p ${OUTPUT_PATH}
-echo "Compressing $MEDIA_VOLUME_PATH into ${MEDIA_ARCHIVE}"
-zip -r "${MEDIA_ARCHIVE}" "${MEDIA_VOLUME_PATH}"
-echo "Dump SQL DB ${DB_ROLE} in ${DB_CONTAINER_ROLE} into ${SQL_ARCHIVE}"
-docker exec "${DB_CONTAINER_ROLE}" pg_dump -U "${DB_ROLE}" | gzip > "${SQL_ARCHIVE}"
+DATE_NOW=$(date -u +"%Y-%m-%dT%H_%M_%S")
+
+if [ -n "${MEDIA_VOLUME_NAME}" ]; then
+  MEDIA_VOLUME_PATH=$(docker volume inspect ${MEDIA_VOLUME_NAME} | sed -ne '/"Mountpoint": "/p' | sed 's/.*"\(\/.*\)".*/\1/')
+  if [ -n "${MEDIA_VOLUME_PATH}" ]; then
+    MEDIA_ARCHIVE="${OUTPUT_PATH}/media_${DATE_NOW}.zip"
+    echo "Compressing $MEDIA_VOLUME_PATH into ${MEDIA_ARCHIVE}"
+    zip -r "${MEDIA_ARCHIVE}" "${MEDIA_VOLUME_PATH}"
+  fi
+fi
+
+if [ -n "${DB_CONTAINER_NAME}" ]; then
+  SQL_ARCHIVE="${OUTPUT_PATH}/db_dump_${DATE_NOW}.sql.gz"
+  echo "Dump SQL DB ${DB_ROLE} in ${DB_CONTAINER_NAME} into ${SQL_ARCHIVE}"
+  docker exec "${DB_CONTAINER_NAME}" pg_dump -U "${DB_ROLE}" | gzip > "${SQL_ARCHIVE}"
+fi
